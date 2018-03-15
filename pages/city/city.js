bda326efff2b2cca7b14b5efeb32eb73997588c9
zhangxing <zhangxing_956@163.com>
2018-03-15 15:34:06 +0800

    up

--- pages/city/city.js
+++ pages/city/city.js
@@ -7,7 +7,11 @@ Page({
    */
   data: {
     isDomestic:true,
-    province: [{ init: 'A', name: '安徽', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }, { init: 'F', name: '福建', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }, { init: 'F', name: '福建', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }, { init: 'G', name: '广东', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }]
+    province: [{ init: 'A', name: '安徽', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }, { init: 'F', name: '福建', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }, { init: 'F', name: '福建', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }, { init: 'G', name: '广东', cities: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城'] }],
+    focus:false,
+    allCity: ['合肥', '安庆', '蚌埠', '亳州', '巢湖', '池州', '滁州', '阜阳', '淮北', '淮南', '黄山', '六安', '马鞍山', '宿州', '铜陵', '芜湖', '宣城','北京','上海','重庆','天津'],
+    matchCity:[],
+    inputText:''
   },
 
   /**
@@ -57,6 +61,51 @@ Page({
    */
   onReachBottom: function () {
   
+  },
+
+  focus(e) {
+    console.log(e)
+    this.setData({
+      focus:true
+    })
+  },
+
+  blur(e) {
+    if (!e.detail.value){
+      this.setData({
+        focus: false
+      })
+    }
+  },
+
+  search(e) {
+    console.log(e.detail.value)
+    //输入框中没有value值时不匹配
+    if (e.detail.value){
+      let reg = new RegExp("" + e.detail.value + "")
+      let match = []
+      for (let i = 0; i < this.data.allCity.length; i++) {
+        if (reg.test(this.data.allCity[i])) {
+          match.push(this.data.allCity[i])
+        }
+      }
+      this.setData({
+        matchCity: match
+      })
+    }
+    else{
+      this.setData({
+        matchCity: []
+      })
+    }
+    
+  },
+
+  clear() {
+    this.setData({
+      inputText:'',
+      matchCity: []
+    })
   },
 
   /**
