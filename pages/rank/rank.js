d5c9d3b41fff81d5abebddd0c4e40492cdae6a82
zhangxing <zhangxing_956@163.com>
2018-03-20 19:50:17 +0800

    up

--- pages/rank/rank.js
+++ pages/rank/rank.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    isCountry:true,
+    rankingInfo: [{ rank: 4, avatar: '', nickName: '昵称几个字', foot: 9999, gold: 9999 }, 
+    { rank: 5, avatar: '', nickName: '昵称几个字', foot: 9998, gold: 999 }, 
+    { rank: 6, avatar: '', nickName: '昵称几个字', foot: 9997, gold: 99 }, 
+    { rank: 7, avatar: '', nickName: '昵称几个字', foot: 995, gold: 929 }, 
+    { rank: 8, avatar: '', nickName: '昵称几个字', foot: 990, gold: 909 }, 
+    { rank: 9, avatar: '', nickName: '昵称几个字', foot: 910, gold: 569 }, 
+    { rank: 10, avatar: '', nickName: '昵称几个字', foot: 900, gold: 109 }, 
+    { rank: 11, avatar: '', nickName: '昵称几个字', foot: 520, gold: 9 }, 
+    { rank: 12, avatar: '', nickName: '昵称几个字', foot: 120, gold: 3009 }, 
+    { rank: 13, avatar: '', nickName: '昵称几个字', foot: 100, gold: 929 }, 
+    { rank: 14, avatar: '', nickName: '昵称几个字', foot: 20, gold: 609 }]
   },
 
   /**
