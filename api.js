54674935c31a58e7fa4a72a0217739d6d16eb2ec
zhangqi <qii_zhang@odao.com>
2018-03-23 14:31:18 +0800

    up

--- api.js
+++ api.js
@@ -32,40 +32,38 @@ class RankType{
     static RECHARGE = 9;
     
 }
+class PostType{
+    
+    static JINGDIAN = 1;
+    
+    static TECHAN = 2;
+    
+}
 //------------classes--------------
-class UserInfo  {
+class Post  {
     constructor(){
     
     
-        //prop type: string
-        this.uid = null;
+        //prop type: number//帖子id
+        this.postId = null;
     
-        //prop type: string
-        this.nickName = null;
+        //prop type: PostType//帖子类型：景点or特产
+        this.type = null;
     
-        //prop type: string
-        this.avatarUrl = null;
+        //prop type: string//帖子内容，为景点或特产的介绍
+        this.content = null;
     
-        //prop type: string
-        this.gender = null;
+        //prop type: 
+        this.name = null;
     
-        //prop type: string
-        this.city = null;
+        //prop type: number//帖子的评论
+        this.score = null;
     
-        //prop type: string
-        this.province = null;
+        //prop type: string//景点或特产图片url
+        this.img = null;
     
-        //prop type: string
-        this.country = null;
-    
-        //prop type: Boolean
-        this.online = null;
-    
-        //prop type: KV[]
-        this.items = null;
-    
-        //prop type: string[]
-        this.friends = null;
+        //prop type: number//评论数
+        this.commentNum = null;
     
         
         
@@ -80,6 +78,26 @@ class RankItem  {
     
         //prop type: number
         this.rank = null;
+    
+        //prop type: userInfo
+        this.userInfo = null;
+    
+        
+        
+    }
+}
+class Provence  {
+    constructor(){
+    
+    
+        //prop type: 
+        this.proLetter = null;
+    
+        //prop type: 
+        this.provence = null;
+    
+        //prop type: 
+        this.citys = null;
     
         
         
@@ -146,7 +164,6 @@ class Base  {
         Object.assign(this, data);
     }
    error(err) {
-
     }
    static GetUID() {
         if (!Base.UID) {
@@ -179,7 +196,6 @@ class Base  {
         this.APP_NAME=appName;
         this.SRV=url || 'https://h5.ddz2018.com/';//https服务器链接
         this.AUTHED=false;
-
         //start
         return new Promise(resolve => {
             wx.checkSession({
@@ -207,13 +223,11 @@ class Base  {
                 }
             });
         });
-
     }
    static UserAuth(code, suc) {
         let req=new Base();
         req.action='weChat.auth';
         req.reqFields=['payload'];
-
         //set data
         req.payload ={code};
         req.fetch().then( res => {
@@ -226,12 +240,10 @@ class Base  {
         let req=new Base();
         req.action='user.login';
         req.reqFields=[ 'info'];
-
         //wx login
         wx.getUserInfo({
             success: info =>{
                 let app=getApp();
-
                 //fetch srv
                 req.info=info.userInfo;
                 req.fetch().then(()=>{
@@ -248,27 +260,22 @@ class Base  {
                 });
             }
         });
-
     }
    static InitWs(wss) {
         //初始化websocket
         this.WSS=wss=wss || `wss://h5.ddz2018.com/${this.APP_NAME}`;//websocket服务器链接
         this.IO=require('./libs/io.js')(wss + `?_sid=${this.SID}&appName=${this.APP_NAME}`);
-
         return new Promise(resolve => {
             this.IO.on('connect', () => {
                 console.log('socket connected')
                 resolve();
             });
-
             this.IO.on('disconnect', msg => {
                 console.log('socket disconnected', msg);
             });
-
             this.IO.on('disconnecting', () => {
                 console.log('socket disconnecting');
             });
-
             this.IO.on('error', () => {
                 console.log('socket error');
             });
@@ -285,6 +292,83 @@ class Base  {
             this.IO.removeAllListeners(a);
         });
     }
+}
+class UserBriefInfo  {
+    constructor(){
+    
+    
+        //prop type: string
+        this.uid = null;
+    
+        //prop type: string
+        this.nickName = null;
+    
+        //prop type: string
+        this.avatarUrl = null;
+    
+        
+        
+    }
+}
+class Comment  {
+    constructor(){
+    
+    
+        //prop type: number//帖子id
+        this.postId = null;
+    
+        //prop type: UserBriefInfo//用户简单信息
+        this.user = null;
+    
+        //prop type: string//评论id
+        this.commentId = null;
+    
+        //prop type: string//评论内容
+        this.content = null;
+    
+        //prop type: number//评论得分
+        this.score = null;
+    
+        //prop type: number//点赞数
+        this.thumbs = null;
+    
+        //prop type: number//创建时间
+        this.time = null;
+    
+        
+        
+    }
+}
+class City  {
+    constructor(){
+    
+    
+        //prop type: 
+        this.cityname = null;
+    
+        //prop type: 
+        this.cityper = null;
+    
+        
+        
+    }
+}
+class PlayerInfo extends Base {
+    constructor(){
+        super();
+        this.action = 'travel.playerinfo';
+    
+        this._playerUid = null;
+        this._info = null;
+        this.reqFields = ["playerUid"];
+        this.resFields = ["info"];
+    }
+    //client input, optional, type: string
+    get playerUid() {return this._playerUid}
+    set playerUid(v) {this._playerUid = v}
+    //server output, type: UserInfo
+    get info() {return this._info}
+    set info(v) {this._info = v}
 }
 class IndexInfo extends Base {
     constructor(){
@@ -319,23 +403,6 @@ class IndexInfo extends Base {
     get unreadMsgCnt() {return this._unreadMsgCnt}
     set unreadMsgCnt(v) {this._unreadMsgCnt = v}
 }
-class PlayerInfo extends Base {
-    constructor(){
-        super();
-        this.action = 'travel.playerinfo';
-    
-        this._playerUid = null;
-        this._info = null;
-        this.reqFields = ["playerUid"];
-        this.resFields = ["info"];
-    }
-    //client input, optional, type: string
-    get playerUid() {return this._playerUid}
-    set playerUid(v) {this._playerUid = v}
-    //server output, type: UserInfo
-    get info() {return this._info}
-    set info(v) {this._info = v}
-}
 class RankInfo extends Base {
     constructor(){
         super();
@@ -361,6 +428,92 @@ class RankInfo extends Base {
     get ranks() {return this._ranks}
     set ranks(v) {this._ranks = v}
 }
+class PostList extends Base {
+    constructor(){
+        super();
+        this.action = 'post.postlist';
+    
+        this._posts = null;
+        this.reqFields = [];
+        this.resFields = ["posts"];
+    }
+    //server output, type: Post[]//服务器返回帖子列表
+    get posts() {return this._posts}
+    set posts(v) {this._posts = v}
+}
+class CommentPost extends Base {
+    constructor(){
+        super();
+        this.action = 'post.commentpost';
+    
+        this.reqFields = [];
+        this.resFields = [];
+    }
+}
+class PostComments extends Base {
+    constructor(){
+        super();
+        this.action = 'post.postcomments';
+    
+        this._comments = null;
+        this.reqFields = [];
+        this.resFields = ["comments"];
+    }
+    //server output, type: Comment[]//该帖子下的评论
+    get comments() {return this._comments}
+    set comments(v) {this._comments = v}
+}
+class ThumbComment extends Base {
+    constructor(){
+        super();
+        this.action = 'post.thumbcomment';
+    
+        this.reqFields = [];
+        this.resFields = [];
+    }
+}
+class UserInfo extends UserBriefInfo {
+    constructor(){
+        super();
+    
+        //prop type: string
+        this.gender = null;
+    
+        //prop type: string
+        this.city = null;
+    
+        //prop type: string
+        this.province = null;
+    
+        //prop type: string
+        this.country = null;
+    
+        //prop type: Boolean
+        this.online = null;
+    
+        //prop type: KV[]
+        this.items = null;
+    
+        //prop type: string[]
+        this.friends = null;
+    
+        
+        
+    }
+}
+class cityList extends Base {
+    constructor(){
+        super();
+        this.action = 'city.citylist';
+    
+        this._provence = null;
+        this.reqFields = [];
+        this.resFields = ["provence"];
+    }
+    //server output, type: Provence[]
+    get provence() {return this._provence}
+    set provence(v) {this._provence = v}
+}
 class RechargeRankInfo extends RankInfo {
     constructor(){
         super();
@@ -378,10 +531,21 @@ class RechargeRankInfo extends RankInfo {
 exports.Season = Season;
 exports.Weather = Weather;
 exports.RankType = RankType;
-exports.UserInfo = UserInfo;
+exports.PostType = PostType;
+exports.Post = Post;
 exports.RankItem = RankItem;
+exports.Provence = Provence;
 exports.Base = Base;
-exports.IndexInfo = IndexInfo;
+exports.UserBriefInfo = UserBriefInfo;
+exports.Comment = Comment;
+exports.City = City;
 exports.PlayerInfo = PlayerInfo;
+exports.IndexInfo = IndexInfo;
 exports.RankInfo = RankInfo;
+exports.PostList = PostList;
+exports.CommentPost = CommentPost;
+exports.PostComments = PostComments;
+exports.ThumbComment = ThumbComment;
+exports.UserInfo = UserInfo;
+exports.cityList = cityList;
 exports.RechargeRankInfo = RechargeRankInfo;
