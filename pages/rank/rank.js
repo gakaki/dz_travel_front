b962f9dca4746c7a1d554d3fe369de3c9fc2b592
956096455@qq.com <956096455@qq.com>
2018-03-23 16:44:00 +0800

    up

--- pages/rank/rank.js
+++ pages/rank/rank.js
@@ -1,6 +1,7 @@
 // pages/rank/rank.js
 import { RankInfo } from '../../api.js';
-const app = getApp()
+const app = getApp();
+let rankType=1,rankSubtype=1;
 
 Page({
 
@@ -8,46 +9,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-    famous:true,
-    foot:false,
-    score:false,
-    isCountry:true,
+    rankType:1,
+    rankSubtype:1,
     toView:'rank0',
     //全国排行榜前三名
-    topThree: [{ rank: 1, avatar: 'https://wx.qlogo.cn/mmopen/vi_32/ODicJCxia34ErfQyhZ7ZHH7iaGSmylmqpgo5goTggk4xnvia07tvicwUNkicQo7xia0JFbtpW74NzQoQ562smbk1Z8k0g/0', nickName: '昵称几个字七个字', foot: 9999, gold: 9999 },
-      { rank: 2, avatar: 'https://wx.qlogo.cn/mmopen/vi_32/ODicJCxia34ErfQyhZ7ZHH7iaGSmylmqpgo5goTggk4xnvia07tvicwUNkicQo7xia0JFbtpW74NzQoQ562smbk1Z8k0g/0', nickName: '昵称几个字五个字', foot: 9999, gold: 9999 },
-      { rank: 3, avatar: 'https://wx.qlogo.cn/mmopen/vi_32/ODicJCxia34ErfQyhZ7ZHH7iaGSmylmqpgo5goTggk4xnvia07tvicwUNkicQo7xia0JFbtpW74NzQoQ562smbk1Z8k0g/0', nickName: '昵称几个字四个字', foot: 9999, gold: 9999 },],
+    topThree: [{ rank: 1, avatar: 'https://wx.qlogo.cn/mmopen/vi_32/ODicJCxia34ErfQyhZ7ZHH7iaGSmylmqpgo5goTggk4xnvia07tvicwUNkicQo7xia0JFbtpW74NzQoQ562smbk1Z8k0g/0', nickName: '昵称几个字七个字', value: 9999, gold: 9999, uid:'aaa' },
+      { rank: 2, avatar: 'https://wx.qlogo.cn/mmopen/vi_32/ODicJCxia34ErfQyhZ7ZHH7iaGSmylmqpgo5goTggk4xnvia07tvicwUNkicQo7xia0JFbtpW74NzQoQ562smbk1Z8k0g/0', nickName: '昵称几个字五个字', value: 9999, gold: 9999, uid:'bbb' },
+      { rank: 3, avatar: 'https://wx.qlogo.cn/mmopen/vi_32/ODicJCxia34ErfQyhZ7ZHH7iaGSmylmqpgo5goTggk4xnvia07tvicwUNkicQo7xia0JFbtpW74NzQoQ562smbk1Z8k0g/0', nickName: '昵称几个字四个字', value: 9999, gold: 9999, uid:'ccc' },],
     //全国排行榜除去前三的排名
-    rankingCountry: [{ rank: 4, avatar: '', nickName: '昵称几个字', foot: 9999, gold: 9999 }, 
-    { rank: 5, avatar: '', nickName: '昵称几个字', foot: 9998, gold: 999 }, 
-    { rank: 6, avatar: '', nickName: '昵称几个字', foot: 9997, gold: 99 }, 
-    { rank: 7, avatar: '', nickName: '昵称几个字', foot: 995, gold: 929 }, 
-    { rank: 8, avatar: '', nickName: '昵称几个字', foot: 990, gold: 909 }, 
-    { rank: 9, avatar: '', nickName: '昵称几个字', foot: 910, gold: 569 }, 
-    { rank: 10, avatar: '', nickName: '昵称几个字', foot: 900, gold: 109 }, 
-    { rank: 11, avatar: '', nickName: '昵称几个字', foot: 520, gold: 9 }, 
-    { rank: 12, avatar: '', nickName: '昵称几个字', foot: 120, gold: 3009 }, 
-    { rank: 13, avatar: '', nickName: '昵称几个字', foot: 100, gold: 929 }, 
-    { rank: 14, avatar: '', nickName: '昵称几个字', foot: 20, gold: 609 }],
+    rankingCountry: [{ rank: 4, avatar: '', nickName: '昵称几个字', value: 9999, gold: 9999, uid: 'aaa' }, 
+      { rank: 5, avatar: '', nickName: '昵称几个字', value: 9998, gold: 999, uid: 'aaa' }, 
+      { rank: 6, avatar: '', nickName: '昵称几个字', value: 9997, gold: 99, uid: 'aaa' }, 
+      { rank: 7, avatar: '', nickName: '昵称几个字', value: 995, gold: 929, uid: 'aaa' }, 
+      { rank: 8, avatar: '', nickName: '昵称几个字', value: 990, gold: 909, uid: 'aaa' }, 
+      { rank: 9, avatar: '', nickName: '昵称几个字', value: 910, gold: 569, uid: 'aaa' }, 
+      { rank: 10, avatar: '', nickName: '昵称几个字', value: 900, gold: 109, uid: 'aaa' }, 
+      { rank: 11, avatar: '', nickName: '昵称几个字', value: 520, gold: 9, uid: 'aaa' }, 
+      { rank: 12, avatar: '', nickName: '昵称几个字', value: 120, gold: 3009, uid: 'aaa' }, 
+      { rank: 13, avatar: '', nickName: '昵称几个字', value: 100, gold: 929, uid: 'aaa' }, 
+      { rank: 14, avatar: '', nickName: '昵称几个字', value: 20, gold: 609, uid: 'aaa' }],
     //好友排行榜
-    rankingFriend: [{ rank: 1, avatar: '', nickName: '昵称几个字', foot: 9999, gold: 9999 },
-    { rank: 2, avatar: '', nickName: '昵称几个字', foot: 9998, gold: 999 },
-    { rank: 3, avatar: '', nickName: '昵称几个字', foot: 9997, gold: 99 },
-    { rank: 4, avatar: '', nickName: '昵称几个字', foot: 995, gold: 929 },
-    { rank: 5, avatar: '', nickName: '昵称几个字', foot: 990, gold: 909 },
-    { rank: 6, avatar: '', nickName: '昵称几个字', foot: 910, gold: 569 },
-    { rank: 7, avatar: '', nickName: '昵称几个字', foot: 900, gold: 109 },
-    { rank: 8, avatar: '', nickName: '昵称几个字', foot: 520, gold: 9 },
-    { rank: 9, avatar: '', nickName: '昵称几个字', foot: 120, gold: 3009 },
-    { rank: 10, avatar: '', nickName: '昵称几个字', foot: 100, gold: 929 },
-    { rank: 11, avatar: '', nickName: '昵称几个字', foot: 20, gold: 609 }],
+    rankingFriend: [{ rank: 1, avatar: '', nickName: '昵称几个字', value: 9999, gold: 9999, uid: 'aaa' },
+      { rank: 2, avatar: '', nickName: '昵称几个字', value: 9998, gold: 999, uid: 'aaa' },
+      { rank: 3, avatar: '', nickName: '昵称几个字', value: 9997, gold: 99, uid: 'aaa' },
+      { rank: 4, avatar: '', nickName: '昵称几个字', value: 995, gold: 929, uid: 'aaa' },
+      { rank: 5, avatar: '', nickName: '昵称几个字', value: 990, gold: 909, uid: 'aaa' },
+      { rank: 6, avatar: '', nickName: '昵称几个字', value: 910, gold: 569, uid: 'aaa' },
+      { rank: 7, avatar: '', nickName: '昵称几个字', value: 900, gold: 109, uid: 'aaa' },
+      { rank: 8, avatar: '', nickName: '昵称几个字', value: 520, gold: 9, uid: 'aaa' },
+      { rank: 9, avatar: '', nickName: '昵称几个字', value: 120, gold: 3009, uid: 'aaa' },
+      { rank: 10, avatar: '', nickName: '昵称几个字', value: 100, gold: 929, uid: 'aaa' },
+      { rank: 11, avatar: '', nickName: '昵称几个字', value: 20, gold: 609, uid: 'aaa' }],
+    //自己的排名
+    selfRank:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getRankInfo();
   },
 
   /**
@@ -78,54 +79,114 @@ Page({
   
   },
 
+  getRankInfo() {
+    let req = new RankInfo();
+    req.rankType = rankType;
+    req.rankSubtype = rankSubtype;
+    req.fetch().then(() => {
+      let ranks = [];
+      let info = req.ranks.userInfo;
+      let selfRank = {};
+      let vKye;
+      //通过rankType来决定需要渲染数据中的value取items中哪个值
+      switch(rankType){
+        case 1:
+          vKye = 13;
+          break;
+        case 2:
+          vKye = 14;
+          break;
+        case 3:
+          vKye = 6;
+          break;
+      }
+      //给selfRank赋值
+      selfRank.rank = req.selfRank.rank;
+      selfRank.value = req.selfRank.userInfo.items.get(vKye);
+      //通过循环返回的数据只取出其中需要的数据再setdata以减少数据传输大小。
+      for (let i =0;i<req.ranks.length;i++){
+        let obj={};
+        obj.rank = req.ranks.rank;
+        obj.avatar = info.avatarUrl;
+        obj.nickName = info.nickName;
+        obj.uid = info.uid;
+        obj.value = info.items.get(vKye);
+        obj.gold = info.items.get(1);
+        ranks[i] = obj;
+      }
+      //全国榜单需要把前三名分开
+      if(rankSubtype ==1){
+        let topThree = ranks.splice(0,3);
+        this.setData({
+          topThree,
+          rankingCountry:ranks,
+        })
+      }
+      else{
+        this.setData({
+          rankingFriend:ranks
+        })
+      }
+    })
+  },
+
   rankCountry() {
+    rankSubtype = 1;
     this.setData({
-      isCountry:true,
+      rankSubtype,
       toView: 'rank0',
     })
+    this.getRankInfo();
   },
 
   rankFriend() {
+    rankSubtype = 2;
     this.setData({
-      isCountry: false,
+      rankSubtype,
       toView: 'rank0',
     })
+    this.getRankInfo();
   },
 
   lookFamous() {
+    rankType = 1;
+    rankSubtype = 1;
     this.setData({
-      famous: true,
-      foot: false,
-      score: false,
-      isCountry:true,
+      rankType,
+      rankSubtype,
       toView: 'rank0',
     })
+    this.getRankInfo();
   },
 
   lookFoot() {
+    rankType = 2;
+    rankSubtype = 1;
     this.setData({
-      famous: false,
-      foot: true,
-      score: false,
-      isCountry: true,
+      rankType,
+      rankSubtype,
       toView: 'rank0',
     })
+    this.getRankInfo();
   },
 
   lookScore() {
+    rankType = 3;
+    rankSubtype = 1;
     this.setData({
-      famous: false,
-      foot: false,
-      score: true,
-      isCountry: true,
+      rankType,
+      rankSubtype,
       toView: 'rank0',
     })
+    this.getRankInfo();
   },
 
-  toOther() {
-    //此处需要传个sid或者uid，后期跟后台沟通
+  toOther(e) {
+    //此处需要传uid，
+    console.log(e)
+    let uid = e.currentTarget.dataset.uid
     wx.navigateTo({
-      url: '../other/other',
+      url: '../other/other?uid='+uid,
     })
   },
 
