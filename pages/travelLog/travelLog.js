f6bf32698dbd8be8536292a4af07b965f6af8e28
yanghongyan <yanghongyan@yanghongyandeMac-mini.local>
2018-03-27 15:58:06 +0800

    up

--- pages/travelLog/travelLog.js
+++ pages/travelLog/travelLog.js
@@ -1,89 +1,65 @@
 // pages/travelLog/travelLog.js
+import { formatTime } from '../../utils/util.js'
+
+
+let data = [{
+  city: '南京',
+  time: '3月15日',
+  car: true,
+  scenicSpots: ['种树领', '秦淮河']
+},
+{
+  time: '3月15日',
+  car: false,
+  scenicSpots: ['种树领']
+},
+{
+  city: '北京',
+  time: '3月15日',
+  car: false,
+  scenicSpots: ['种树领', '秦淮河heheheeehh']
+},
+{
+  time: '3月15日',
+  car: true,
+  scenicSpots: ['种树领', '秦淮河heheheeehh']
+},
+{
+  city: '苏州',
+  time: '3月15日',
+  car: false,
+  scenicSpots: ['种树领', '秦淮河heheheeehh']
+}]
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-      init:[{
-        city:'南京',
-        time:'3月15日',
-        car:true,
-        scenicSpots:['种树领','秦淮河']
-      }, 
-      {
-        time: '3月15日',
-        car:false,
-        scenicSpots: ['种树领','秦淮河heheheeehh']
-      }, 
-      {
-        city: '北京',
-        time: '3月15日',
-        car: false,
-        scenicSpots: ['种树领', '秦淮河heheheeehh']
-      }, 
-      {
-        time: '3月15日',
-        car: true,
-        scenicSpots: ['种树领', '秦淮河heheheeehh']
-      }, 
-      {
-        city: '苏州',
-        time: '3月15日',
-        car: false,
-        scenicSpots: ['种树领', '秦淮河heheheeehh']
-      }]
+      
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    for(let i in data) {
+      data[i].time = this.formatTime('2018-3-5',1);
+    }
+    this.setData({
+      init: data
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  formatTime(time,type){
+     let arr = time.spilt('-')
+     if (dateType == 1) {
+       return arr.join('/')
+     }
+     return arr[0] + '月' + day + '日'
+     
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
   /**
    * 用户点击右上角分享
    */
