312cfd4d5741acbd6f1ce5b226b6b67ce7dd3177
zhangqi <qii_zhang@odao.com>
2018-03-28 17:14:30 +0800

    up

--- pages/play/play.js
+++ pages/play/play.js
@@ -28,7 +28,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.setNavigationBarTitle({
+      title: '成都游玩'
+    })
   },
   // 划线的方法集
   objTostring(obj) {
--- pages/recharge/recharge.js
+++ pages/recharge/recharge.js
@@ -5,16 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-  goldInfo: sheet.pays,
-  gCount: 0,
-  mCount: 0,
-  pop: false
+    goldInfo: sheet.pays,
+    gCount: 0,
+    mCount: 0,
+    pop: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '充值商城'
+    })
     console.log(this.data.goldInfo)
   },
   hide() {
@@ -23,7 +26,7 @@ Page({
     })
   },
   toBuy() {
-this.hide()
+    this.hide()
   },
   isBuy(e) {
     let obj = this.data.goldInfo[e.currentTarget.dataset.idx]
@@ -32,54 +35,54 @@ this.hide()
       mCount: obj.pay,
       pop: true
     })
-console.log(e)
+    console.log(e)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
