4a31a01c21eed1c18eb603132d6b614caa2f18f6
yhy <405939579@qq.com>
2018-03-29 14:32:51 +0800

    接个人信息接口

--- pages/self/self.js
+++ pages/self/self.js
@@ -1,34 +1,8 @@
 // pages/self/self.js
 import { getUserInfo } from '../../utils/util.js';
 import { PlayerInfo } from '../../api.js';
+import { Item, items } from '../../sheets.js';
 const app = getApp();
-let obj = {
-    a: {
-      title: '累计获得城市积分',
-  num: '555'
-},
-  b: {
-      title: '收集明信片数量',
-    num: '555'
-  },
-  c: {
-      title: '发表评论数量',
-    num: '555'
-  },
-  d: {
-      title: '获得点赞数量',
-    num: '555'
-  }
-  ,
-  e: {
-      title: '获得特产数量',
-    num: '555'
-  },
-  f: {
-      title: '获得特产数量',
-    num: '555'
-  } 
-}
 
 Page({
 
@@ -62,60 +36,17 @@ Page({
     this.setData({userInfo})
     let m = new PlayerInfo();
     m.fetch().then(res=>{
-      this.setData({
-        list: res.info
-      })
       console.log(res.info)
-
+      this.setData({
+        init: res.info,
+        gold: res.info.items[Item.GOLD],
+        integral: res.info.items[Item.POINT]
+      })
+      
+      
     })
-    // console.log(obj)
-    // this.setData({
-    //   list:Object.values(obj)
-    // }) 
   },
   
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
 
   /**
    * 用户点击右上角分享
