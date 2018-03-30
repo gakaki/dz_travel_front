81daceab003dae78d15571945bb0d2949f93b6ff
zhangqi <qii_zhang@odao.com>
2018-03-30 10:17:44 +0800

    up

--- components/walk/walk.js
+++ components/walk/walk.js
@@ -28,43 +28,75 @@ Component({
   data: {
     animationData: {},
     left: 0,
-    walkInfo: { x: 0, y: 0, tX: 100, tY: 100, time: 3000 }  // time为未来的时间戳
+    // walkInfo: { x: 0, y: 0, tX: 100, tY: 100, time: 3000 }  // time为未来的时间戳
+    walkInfo: { x: 0, y: 0, time: 3000 }
   },
   attached() {
 
     //let st = Base.servertime  
-    let st = 5500
+    // let st = 5500
+    // walkInfoObj =  this.properties.walkInfoArr.find((v,i)=>{
+    //   return v.time>st
+    // })
+    // let lastObj = this.properties.walkInfoArr[walkInfoObj.idx-1]
+    // //一段路走过的百分比
+    // let per = (st - lastObj.time) / (walkInfoObj.time - lastObj.time)
+    // curPoint.x = parseInt( walkInfoObj.x + (walkInfoObj.tX - walkInfoObj.x)*per)
+    // curPoint.y = parseInt(walkInfoObj.y + (walkInfoObj.tY - walkInfoObj.y) * per)
+    // restTime = parseInt(lastObj.time + (walkInfoObj.time - lastObj.time)*per)
+
+
+     let st = 5500
     walkInfoObj =  this.properties.walkInfoArr.find((v,i)=>{
       return v.time>st
     })
     let lastObj = this.properties.walkInfoArr[walkInfoObj.idx-1]
     //一段路走过的百分比
     let per = (st - lastObj.time) / (walkInfoObj.time - lastObj.time)
-    curPoint.x = parseInt( walkInfoObj.x + (walkInfoObj.tX - walkInfoObj.x)*per)
-    curPoint.y = parseInt(walkInfoObj.y + (walkInfoObj.tY - walkInfoObj.y) * per)
+    curPoint.x = parseInt(lastObj.x + (walkInfoObj.x - lastObj.x)*per)
+    curPoint.y = parseInt(lastObj.y + (walkInfoObj.y - lastObj.y) * per)
     restTime = parseInt(lastObj.time + (walkInfoObj.time - lastObj.time)*per)
   },
   ready() {
     //行走动画
-    timer = setInterval(() => {
+    // timer = setInterval(() => {
+    //   let l = this.data.left - 38 == -228 ? 0 : this.data.left - 38
+    //   this.setData({
+    //     left: l
+    //   })
+    // }, 100)
+    // let fObj = { x: walkInfoObj.x, y: walkInfoObj.y, tX: curPoint.x, tY: curPoint.y, time: 1}
+    // this.setData({
+    //   walkInfo: fObj
+    // })
+    // this.move(this.data.walkInfo)
+    // setTimeout(()=>{
+    //   let obj = { x: curPoint.x, y: curPoint.y, tX: walkInfoObj.tX, tY: walkInfoObj.tY, time: restTime}
+    //   this.setData({
+    //     walkInfo: obj
+    //   })
+    //   this.move(this.data.walkInfo)
+    // },100)
+   
+
+   timer = setInterval(() => {
       let l = this.data.left - 38 == -228 ? 0 : this.data.left - 38
       this.setData({
         left: l
       })
     }, 100)
-    let fObj = { x: walkInfoObj.x, y: walkInfoObj.y, tX: curPoint.x, tY: curPoint.y, time: 1}
+   let fObj = { x: lastObj.x, y: lastObj.y, tX: curPoint.x, tY: curPoint.y, time: 1}
     this.setData({
       walkInfo: fObj
     })
     this.move(this.data.walkInfo)
     setTimeout(()=>{
-      let obj = { x: curPoint.x, y: curPoint.y, tX: walkInfoObj.tX, tY: walkInfoObj.tY, time: restTime}
+      let obj = { x: curPoint.x, y: curPoint.y, tX: walkInfoObj.x, tY: walkInfoObj.y, time: restTime}
       this.setData({
         walkInfo: obj
       })
       this.move(this.data.walkInfo)
     },100)
-   
   },
  
   detached() {
